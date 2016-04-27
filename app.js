67acba10eb656c997d3a3e787af364f5fee776f7
Joel <joelahlstedt@gmail.com>
2016-04-27 18:03:48 +0300

    Improve dodging
    Now the bot thinks opponents as a snake rather than a dot (opponent's head)

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function SlitherBot() {
 
 	this.botOn = true;
 	this.currentDir = 0;
+	this.hideAll = false;
+	this.lastTurned = 0;
 
 	this.setDirection = function(deg) {
 		if(deg > 240) deg = deg - 240;
@@ -34,6 +36,8 @@ function SlitherBot() {
 		this.log("Started the bot!");
 		
 		this.setDirection(this.currentDir);
+
+		this.autoTasks();
 	}
 
 	this.turnAround = function() {
@@ -46,18 +50,51 @@ function SlitherBot() {
 
 	this.mySnake = snake;
 
-	this.getNearestSnake = function() {
+	this.getNearestSnake = function(callback) {
 		this.snakeList = snakes;
 		this.mySnake = snake;
+
+		snakeList = this.snakeList;
+		mySnake = this.mySnake;
+
 		var blocks = 0;
-		for(var i = 0; i < this.snakeList.length; i++) {
-			if(this.mySnake.xx == this.snakeList[i].xx && this.mySnake.yy == this.snakeList[i].yy) /**/;
-			else {
-				var tblocks = (Math.abs(this.mySnake.xx - this.snakeList[i].xx) + Math.abs(this.mySnake.yy - this.snakeList[i].yy)) / 2;
-				if(tblocks < blocks || blocks == 0) blocks = tblocks;
+		var sId = 0;
+		for(var i = 0; i < snakeList.length; i++) {
+			if(snakeList[i].xx != mySnake.xx && snakeList[i].yy != mySnake.yy) {
+				var tblocks = (Math.abs(mySnake.xx - snakeList[i].xx) + Math.abs(mySnake.yy - snakeList[i].yy)) / 2;
+				if(tblocks < blocks || blocks == 0) {
+					blocks = tblocks;
+					sId = snakeList[i].id;
+				}
+
+				for(var j = 0; j < snakeList[i].pts.length; j++) {
+					var tblocks = (Math.abs(mySnake.xx - snakeList[i].pts[j].xx) + Math.abs(mySnake.yy - snakeList[i].pts[j].yy)) / 2;
+					if(tblocks < blocks) {
+						blocks = tblocks;
+						sId = snakeList[i].id;
+					}
+				}
 			}
 		}
-		return blocks;
+
+		callback({ blocksAway: blocks, thickness: 0, snakeId: sId }); // todo
+	}
+
+	this.iWantAll = function() {
+		localStorage.edttsg = 1;
+		document.getElementById("fbh").style.display = "none";
+		document.getElementById("twth").style.display = "none";
+		document.getElementById("grqh").style.display = "none";
+		document.getElementById("clq").style.display = "none";
+		document.getElementById("tips").style.display = "none";
+		document.getElementById("logo").style.display = "none";
+		document.getElementsByTagName("iframe")[0].style.display = "none";
+		setInterval(function() {
+			document.getElementById("login").style.transform = "scale(1, 1)";
+		}, 1000);
+		window.oncontextmenu = function() {
+			return true;
+		};
 	}
 
 	this.getNextDirection = function() {
@@ -66,20 +103,41 @@ function SlitherBot() {
 
 	this.autoBot = function() {
 		var parent = this;
-		setInterval(function() {
-			if(parent.getNearestSnake() < 80) {
-				parent.turnAround();
-			}
-			console.log(parent.getNearestSnake());
-		}, 1000);
+		var latestSnakeTurnedOn = 0;
+		this.log("Started autoBot!");
+		function doBot() {
+			parent.getNearestSnake(function(data) {
+				document.getElementsByClassName("nsi")[19].innerHTML = 'Nearest snake: ' + Math.round(data.blocksAway / 50) + " blocks away";
+				if(data.blocksAway < (data.thickness + 210) && data.blocksAway != 0 && ((((Date.now() / 1000) % 60) - parent.lastTurned) > 4 || data.snakeId != latestSnakeTurnedOn)) {
+					parent.lastTurned = (Date.now() / 1000) % 60;
+					parent.turnAround();
+				}
+				setTimeout(function(){doBot();}, 500);
+			});
+		}
+		doBot();
 	}
 
 	this.log = function(txt) {
 		console.info("[SlitherBot] [" + version + "] " + txt);
 	}
+
+	this.autoTasks = function() {
+		var parent = this;
+		setInterval(function() {
+			if(parent.hideAll) parent.hideSnakes();
+		}, 500);
+	}
+
+	this.hideSnakes = function() {
+		this.snakes = snakes; // i want to keep 'em!
+		snakes = [snake];
+	}
 }
 
 var bot = new SlitherBot();
+
+bot.iWantAll();
 
 window.onmousemove = null;
 
@@ -90,5 +148,7 @@ document.onkeypress = function(e) {
 		bot.bot();
 	} else if(String.fromCharCode(e.which) == "s") {
 		bot.autoBot();
+	} else if(String.fromCharCode(e.which) == "h") {
+		bot.hideAll = true;
 	}
 }
